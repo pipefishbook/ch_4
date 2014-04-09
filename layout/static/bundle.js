9153d0cc2ba82c3855403047c3d36b3a14734b6c
Patrick Mulder <mulder.patrick@gmail.com>
2014-04-09 22:14:37 +0200

    non push

--- layout/static/bundle.js
+++ layout/static/bundle.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
   console.log('init');
   var router = new MoviesRouter({el: $('#movies') });
   Backbone.history.start({
-    pushState: true,
+    pushState: false,
     root: '/'
   });
 });
@@ -101,7 +101,7 @@ var Backbone = require('backbone');
 var ChoseView = Backbone.View.extend({
 
   template: '<h1>Welcome to Munich Cinema</h1>\
-             <h2>Please chose a movie</h2>',
+             <h2>Please choose a movie</h2>',
 
   class: 'details',
   render: function() {
@@ -194,7 +194,7 @@ var _ = require('underscore');
 var MovieView = Backbone.View.extend({
   tagName: 'article',
   className: 'movie',
-  template: '<h1><a href="/movies/<%= id %>"><%= title %></a><hr></h1>',
+  template: '<h1><a href="#movies/<%= id %>"><%= title %></a><hr></h1>',
 
   events: {
     'click': 'selectMovie'
@@ -204,7 +204,7 @@ var MovieView = Backbone.View.extend({
     // console.log($(ev.currentTarget).html());
     console.log('event on ' + this.model.id);
     if (!this.model.get('selected')) {
-      this.router.navigate("/movies/" + this.model.id, {trigger: true});
+      this.router.navigate("#movies/" + this.model.id, {trigger: true});
     }
   },
  
